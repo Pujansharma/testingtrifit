f26a45939e5634fee6bb8d19255a64872c5a9543
pujansharma <sharmapujan209@gmail.com>
2025-04-08 18:01:21 +0530

    testing

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: "https://testingtrifit.onrender.com/auth/google/callback",
     passReqToCallback: true
   },
   (req, accessToken, refreshToken, profile, done) => {
